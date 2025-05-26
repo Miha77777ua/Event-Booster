0b14b50cdf1ab24a281b475a7c5105a1206d6482
vladyslava-martyniuk <vlada.martyniuk@gmail.com>
2025-05-26 16:42:43 +0300

    fixed

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,22 +1,7 @@
-const toggle = document.querySelector('.dropdown-toggle');
-    const menu = document.querySelector('.dropdown-menu');
-    const items = document.querySelectorAll('.dropdown-menu div');
-
-    toggle.addEventListener('click', () => {
-      menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+const opinions = document.querySelectorAll(".options li");
+const summary = document.querySelector(".dropdown summary");
+opinions.forEach((el) => {
+    el.addEventListener("click", () => {
+        summary.textContent = el.textContent;
     });
-
-    items.forEach(item => {
-      item.addEventListener('click', () => {
-        toggle.textContent = item.textContent;
-        toggle.setAttribute('data-value', item.getAttribute('data-value'));
-        menu.style.display = 'none';
-        console.log('Selected:', item.getAttribute('data-value')); // для перевірки
-      });
-    });
-
-    document.addEventListener('click', (e) => {
-      if (!e.target.closest('.header__input-filter')) {
-        menu.style.display = 'none';
-      }
-    });
+});
