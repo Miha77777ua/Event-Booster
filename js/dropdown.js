c242459b3bca630db24a6d4e7a8ef00e0deb50e7
vladyslava-martyniuk <vlada.martyniuk@gmail.com>
2025-05-26 19:11:15 +0300

    done part of filling contries

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,7 +1,14 @@
-const opinions = document.querySelectorAll(".options li");
-const summary = document.querySelector(".dropdown summary");
-opinions.forEach((el) => {
-    el.addEventListener("click", () => {
-        summary.textContent = el.textContent;
+export function initDropdown() {
+    const opinions = document.querySelectorAll(".options li");
+    const dropdown = document.querySelector(".dropdown");
+    const summary = document.getElementById("selected-option"); 
+
+    if (!opinions.length || !summary || !dropdown) return;
+
+    opinions.forEach((el) => {
+        el.addEventListener("click", () => {
+            summary.textContent = el.textContent;
+            dropdown.removeAttribute("open");
+        });
     });
-});
+}
--- js/index.js
+++ js/index.js
@@ -1,2 +1,6 @@
 import cards from "./cards.js";
+import { initDropdown } from "./dropdown.js";
+
+initDropdown();
+
 cards(1);
