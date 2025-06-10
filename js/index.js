a41eb2399b20cad38c26114147046f7106a24190
Ivan <turchinivan185@school91.org.ua>
2025-06-10 19:14:15 +0300

    final modal

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 import { cards } from "./cards.js";
 import { setupDropdown, setupSearch } from "./filter.js";
+import { renderModalData } from "./modal.js";
 
 setupDropdown();
 setupSearch();
 cards(1);
+renderModalData();
